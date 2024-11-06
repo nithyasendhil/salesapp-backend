c48d0c22911d3de21a1e34dc5f0949463165c3c9
nithyasendhil <nithya.s@gembaconcepts.com>
2024-11-06 15:56:04 +0530

    Initial commit

---
+++ src/main/java/com/indona/invento/salesapp_backend/SalesappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.indona.invento.salesapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indona/invento/salesapp_backend/SalesappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indona.invento.salesapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
